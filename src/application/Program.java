108b12b48f8e36d9a8ef7ff3f574730167502a4f
André Cahango <misterdel04@hotmail.com>
2023-09-20 14:13:43 +0100

    clear screen and move piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,39 @@
 package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 public class Program {
 
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
-		
-		UI.printBoard(chessMatch.getPieces());
-		
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.println("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.println("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException ce) {
+				System.out.println(ce.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException ie) {
+				System.out.println(ie.getMessage());
+				sc.nextLine();
+			}
+		}
+
 	}
 
 }
